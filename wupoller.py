01c49ae66623ab9ffff5b098faad7438e6f1d06a
Andrew Bulkeley <gochuonpire@gmail.com>
2017-09-13 19:39:58 -0400

    Made it actually work

--- wupoller.py
+++ wupoller.py
@@ -1,14 +1,14 @@
 import requests
 import _mysql as MySQLdb
 import time
+from dateutil import parser
 
 # Polls Weather Underground and stores some data
 while True:
   print 'Polling Weather Underground'
   data = requests.get('http://api.wunderground.com/api/b5745e9cacaf559d/conditions/q/VA/Great_Falls.json').json()
 
-  cnx = MySQLdb.connection(host="localhost", user="root", passwd="raspberry", db="home")
-
+  cnx = MySQLdb.connect(host="localhost", user="root", passwd="raspberry", db="home")
   cursor = cnx.cursor()
   ts = time.time()
   timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -17,6 +17,9 @@ while True:
                   " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
   observation_time = data['current_observation']['observation_time_rfc822']
+  data_date = parser.parse(observation_time)
+  final_time = data_date.strftime('%Y-%m-%d %H:%M:%S')
+  print final_time
   temp_f = data['current_observation']['temp_f']
   humidity = data['current_observation']['relative_humidity']
   pressure = data ['current_observation']['pressure_mb']
@@ -26,10 +29,10 @@ while True:
   solarradiation = data ['current_observation']['solarradiation']
   precip_1hr_in = data ['current_observation']['precip_1hr_in']
 
-  update_data = (time, date, temp_f, humidity, pressure, wind_degrees, wind_mph, wind_gust_mph, solarradiation, precip_1hr_in)
+  update_data = (timestamp, final_time, temp_f, humidity, pressure, wind_degrees, wind_mph, wind_gust_mph, solarradiation, precip_1hr_in)
   cursor.execute(update_insert, update_data)
   cnx.commit()
   cnx.close()
 
   print 'Done'
-  time.sleep(300.0 - ((time.time() - starttime) % 60.0))
+  time.sleep(300)
