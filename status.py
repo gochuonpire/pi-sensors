49e38e5c8789d168faaf4867a8f8dc7cf4b6dbde
Andrew Bulkeley <gochuonpire@gmail.com>
2017-09-14 13:45:21 -0400

    indents are hard

--- status.py
+++ status.py
@@ -23,7 +23,7 @@ for sensor_ip in r:
       if "pi/sensor.py" in line:
         print '\033[92m' + host + " is online and sensor process is running"
         online.append(host)
-    elif host not in online:
+      elif host not in online:
         print '\033[93m' + host + " is online but no sensor process running"
   except:
     print '\033[91m' + "Could not connect to " + host
